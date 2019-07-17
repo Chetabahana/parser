b6819dc03bde6c67a47bdae9f428cc81d542fe61
Chetabahana <chetabahana@gmail.com>
2019-07-17 19:47:30 +0700

    Update update.sh

--- scripts/gcb/docker/update.sh
+++ scripts/gcb/docker/update.sh
@@ -56,9 +56,11 @@ UPSTREAM=https://github.com/mirumee/saleor.git
 cd $HOME && rm -rf $REPO
 git clone $ORIGIN && cd $REPO
 
-git checkout master
-git remote add upstream $UPSTREAM
+#delete all branches except master 
+git branch | grep -v "master" | xargs git branch -D
+[ `git rev-parse --abbrev-ref HEAD` != master ] && git checkout master
 
+git remote add upstream $UPSTREAM
 git pull --rebase upstream master
 git reset --hard upstream/master
 
