066a6068ddeb2821ce84cb444e717f614b081085
Chetabahana <chetabahana@gmail.com>
2019-07-17 19:54:10 +0700

    Update update.sh

--- scripts/gcb/docker/update.sh
+++ scripts/gcb/docker/update.sh
@@ -57,8 +57,8 @@ cd $HOME && rm -rf $REPO
 git clone $ORIGIN && cd $REPO
 
 #delete all branches except master 
-git branch | grep -v "master" | xargs git branch -D
 [ `git rev-parse --abbrev-ref HEAD` != master ] && git checkout master
+git branch | grep -v "master" | xargs git branch -D
 
 git remote add upstream $UPSTREAM
 git pull --rebase upstream master
