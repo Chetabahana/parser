7300218bdff0fe56810a64e06ce7e989d9e02934
chetabahana <chetabahana@gmail.com>
2019-08-01 00:38:36 +0700

    cron update on 2019-08-01 00:38

--- .codefresh/pipeline/builders/python/artifact.sh
+++ .codefresh/pipeline/builders/python/artifact.sh
@@ -19,3 +19,18 @@ ls -al /
 echo "\n$hr\nCURRENT REPOSITORY\n$hr"
 pwd
 ls -al .
+
+echo "\n$hr\nCLONE ORIGIN\n$hr"
+REPO=$(basename $ORIGIN .git)
+rm -rf $REPO && git clone $ORIGIN $REPO
+find .io -type d -name $REPO -exec cp -frpvT {} $REPO \;
+
+FLOWNAME=Toko-Chetabahana
+WORKFLOW=$FLOWNAME/branches/.google
+FLOW_GIT=https://github.com/MarketLeader/$FLOWNAME.git
+
+rm -rf $FLOWNAME && git clone $FLOW_GIT
+cp -frpvT $WORKFLOW $REPO/.google
+
+echo "\n$hr\nORIGIN REPOSITORY\n$hr"
+ls -al $REPO
