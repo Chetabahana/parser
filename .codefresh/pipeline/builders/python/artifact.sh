37490f3328b059a6e052e46e404af8c68cc397c7
chetabahana <chetabahana@gmail.com>
2019-08-01 06:22:32 +0700

    cron update on 2019-08-01 06:22

--- .codefresh/pipeline/builders/python/artifact.sh
+++ .codefresh/pipeline/builders/python/artifact.sh
@@ -23,6 +23,13 @@ ls -al .
 echo "\n$hr\nCLONE ORIGIN\n$hr"
 REPO=$(basename $ORIGIN .git)
 rm -rf $REPO && git clone $ORIGIN $REPO
+
+cd $REPO
+git pull origin master
+git reset --hard origin/master
+cd ..
+
+echo "\n$hr\nCOPYING\n$hr"
 find .io -type d -name $REPO -exec cp -frpvT {} $REPO \;
 
 FLOWNAME=Toko-Chetabahana
@@ -35,6 +42,8 @@ cp -frpvT $WORKFLOW $REPO/.google
 echo "\n$hr\nORIGIN REPOSITORY\n$hr"
 cd $REPO && ls -al .
 
+[ $HOME = /root ] && return
+echo "\n$hr\nPUSH REPOSITORY\n$hr"
 ln -s $HOME/.ssh/push /bin/push
 chmod +x /bin/push
 push $ORIGIN
